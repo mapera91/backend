d009d97d8420055a9e2a671925436c531e5c9310
mapera91 <pikitamaria@gmail.com>
2018-05-03 12:53:10 +0200

    clientes

--- routes/cliente.js
+++ routes/cliente.js
@@ -5,11 +5,53 @@ var Cliente = require('../models/cliente');
 
 var app = express();
 
-app.get('/:nombre',(req,res,next)=> {
+app.get('/nombre/:nombre',(req,res,next)=> {
 
     var nombre = req.params.nombre;
     
     Cliente.find({nombre:{$regex:nombre,$options:'i'}}).exec((err,clientes)=> {
+        if(err) {
+            return res.status(500).json({
+                ok:false,
+                mensaje:'Error de acceso a DB',
+                errores:err
+            })
+        }
+        res.status(200).json({
+            ok:true,
+            clientes:clientes
+        })
+    });
+
+});
+
+app.get('/localidad/:localidad',(req,res,next)=> {
+
+    var localidad = req.params.localidad;
+    
+    Cliente.find({localidad:{$regex:localidad,$options:'i'}}).exec((err,clientes)=> {
+        if(err) {
+            return res.status(500).json({
+                ok:false,
+                mensaje:'Error de acceso a DB',
+                errores:err
+            })
+        }
+        res.status(200).json({
+            ok:true,
+            clientes:clientes
+        })
+    });
+
+});
+
+app.get('/mixto/:nombre/:localidad',(req,res,next)=> {
+
+    var nombre = req.params.nombre;
+    var localidad = req.params.localidad;
+    
+    //Cliente.find({$or:[{nombre:{$regex:nombre,$options:'i'}},{localidad:{$regex:localidad,$options:'i'}}]}).exec((err,clientes)=> {
+    Cliente.find({nombre:{$regex:nombre,$options:'i'},localidad:{$regex:localidad,$options:'i'}}).exec((err,clientes)=> {
         if(err) {
             return res.status(500).json({
                 ok:false,
